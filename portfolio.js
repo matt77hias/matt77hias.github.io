abaac08526f67ec8b3710c48fc5a93ab66b336b5
ALIEN <matthias.moulin@gmail.com>
2016-10-05 15:50:09 +0200

    pippy instead op pipy

--- portfolio.js
+++ portfolio.js
@@ -204,11 +204,11 @@ addPublication("Efficient Visibility Heuristics for kd-trees Using the RTSAH",
 	);
 
 // 1st Semester - 2nd PhD of Science in Engineering (2016-2017)
-addProject("pipy",
+addProject("pippy",
     ["Matthias Moulin"],
     "October 2016",
-    "res/Projects/pipy/Thumbnail.png", 10, 2016,
-    "https://github.com/matt77hias/pipy",
+    "res/Projects/pippy/Thumbnail.png", 10, 2016,
+    "https://github.com/matt77hias/pippy",
 	[]
 	);
 	
