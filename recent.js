2279ac31a0ebff8642523ecc8f536a8de813be7f
REDDRAGON <matthias.moulin@gmail.com>
2017-08-11 11:37:04 +0200

    Update

--- recent.js
+++ recent.js
@@ -6,20 +6,6 @@ function constructRecent() {
         d = b[h];
         a = document.createElement("div");
         a.className = "recent-container";
-        e = document.createElement("p");
-        e.innerHTML = '<h3 class="recent-title"><a href="' + d.projectpage + '">' + d.title + "</a></h3>";
-        a.appendChild(e);
-        k = document.createElement("h4");
-        k.className = "recent-authors";
-        a.appendChild(k);
-        //for (g = 0; g < d.authors.length; g += 1) {
-        //    f = getAuthor(d.authors[g]);
-        //    if (f.personalPageLink)         { k.innerHTML += '<a href="' + f.personalPageLink + '">' + f.fullName + "</a>" }
-        //    else                            { k.innerHTML += f.fullName }
-        //    if (g === d.authors.length - 1) { k.innerHTML += "."        }
-        //    else                            { k.innerHTML += ", "       }
-        //}
-        //a.appendChild(k);
         c = document.createElement("a");
         c.href = d.projectpage;
         l = document.createElement("img");
