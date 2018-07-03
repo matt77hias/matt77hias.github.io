0d130421010d49c5b54386c0a919650f284196f9
REDDRAGON <matthias.moulin@gmail.com>
2018-07-03 12:56:52 +0200

    Removed jQuery.ready from icons (too slow)

--- js/security.js
+++ js/security.js
@@ -1,13 +1,11 @@
 "use strict";
 
-$( document ).ready(function() {
-	$('a.icon-mail').on('click', function(){
-		var href = $(this).attr('href');
-		$(this).attr('href', href.replace('badmail.', ''));
-	});
+$('a.icon-mail').on('click', function(){
+	var href = $(this).attr('href');
+	$(this).attr('href', href.replace('badmail.', ''));
+});
 
-	$('a.icon-skype').on('click', function(){
-		var href = $(this).attr('href');
-		$(this).attr('href', href.replace('badmail.', ''));
-	});
+$('a.icon-skype').on('click', function(){
+	var href = $(this).attr('href');
+	$(this).attr('href', href.replace('badmail.', ''));
 });
