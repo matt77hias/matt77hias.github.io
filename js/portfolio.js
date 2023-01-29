ae7c27a54798ff472a891304fc1fe260d654b33b
Matthias Moulin <matthias.moulin@gmail.com>
2023-01-29 11:49:16 +0100

    Update portfolio.js

--- js/portfolio.js
+++ js/portfolio.js
@@ -385,7 +385,7 @@ CreatePublication("Efficient Visibility Heuristics for kd-trees Using the RTSAH"
 //-----------------------------------------------------------------------------
 	
 //-----------------------------------------------------------------------------
-// PhD of Science in Engineering (2016-2017) - y2.s2
+// PhD of Science in Engineering - y2.s2
 //-----------------------------------------------------------------------------
 
 CreateProject("MAGE v0",
