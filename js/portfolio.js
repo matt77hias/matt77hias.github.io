3a0e69d4c8c0f5f616e6150ece009b0277757bc5
Matthias Moulin <matthias.moulin@gmail.com>
2025-07-15 10:37:59 +0200

    Update portfolio.js

--- js/portfolio.js
+++ js/portfolio.js
@@ -276,7 +276,7 @@ CreateAuthor("Ruben Pieters"      , "https://rubenpieters.github.io");
 CreatePost("Public Frostbite Archive",
     ["Matthias Moulin"],
     "July 2025", 1, 2025,
-    "https://matt77hias.github.io/blog/2025-07-01-public-frostbite-archive.html");
+    "https://matt77hias.github.io/blog/2025/07/01/public-frostbite-archive.html");
 
 CreatePost("🧙 MAGE: Asserts",
     ["Matthias Moulin"],
